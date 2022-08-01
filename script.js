61fa49390a045501b3375f2ecc18369356a8f8aa
SNHays22 <codergrl22@outlook.com>
2022-08-01 00:28:10 -0500

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 // Write your JavaScript code here!
 
+const { myFetch, pickPlanet, addDestinationInfo, formSubmission } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
-   let listedPlanets;
+    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+        let planet = pickPlanet(listedPlanets);
+
+        return addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
+        
+    });
+
+    let form = document.getElementsByTagName("form");
+    form.addEventListener("submit", function(event){
+
+        event.preventDefault();
+
+        let list = document.getElementsById("faultyItems");
+
+        let pilotInput = document.querySelector("input[name=pilotName]");
+        let pilot = pilotInput.value;
+
+        let copilotInput = document.querySelector("input[name=copilotName]");
+        let copilot = copilotInput.value;
+
+        let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+        let fuelLevel = fuelLevelInput.value;
+
+        let cargoLevelInput = document.querySelector("input[name=cargoMass]");
+        let cargoLevel = cargoLevelInput.value;
+
+        
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+        
+    });
+
 });
